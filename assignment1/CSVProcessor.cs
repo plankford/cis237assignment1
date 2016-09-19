1d4c36739918aab1be4dd4e580efeb985302dff5
Pat Lankford <plankford@secantcorp.com>
2016-09-19 09:04:05 -0400

    Added switch statement to control user input

---
+++ assignment1/CSVProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace assignment1
+{
+    class CSVProcessor
+    {
+        StreamReader streamReader = null;
+
+        
+    }
+}
--- assignment1/Program.cs
+++ assignment1/Program.cs
@@ -16,7 +16,35 @@ namespace assignment1
             //Print the menu to the screen for the user to make a selection
             int userChoice = ui.GetUserInput();
 
-
+            while(userChoice != 5)
+            {
+                switch (userChoice)
+                {
+                case 1:
+                    //Load wine list
+                    Console.WriteLine("Choice 1");
+                    break;
+                case 2:
+                    //Print wine list
+                    Console.WriteLine("Choice 2");
+                    break;
+                case 3:
+                    //Search wine list
+                    Console.WriteLine("Choice 3");
+                    break;
+                case 4:
+                    //Add item to wine list
+                    Console.WriteLine("Choice 4");
+                    break;
+                case 5:
+                    //Close the application
+                    Console.WriteLine("Choice 5");
+                    break;
+                default:
+                    break;
+                }
+                userChoice = ui.GetUserInput();
+            }
         }
     }
 }
